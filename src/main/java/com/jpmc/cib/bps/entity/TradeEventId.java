40a41ce6b581067c4aa7851b3d2a4d99b8c08670
Dharmender Singh <dharmender.764@gmail.com>
2022-07-31 19:08:18 +0530

    Update TradeEventId.java

--- src/main/java/com/jpmc/cib/bps/entity/TradeEventId.java
+++ src/main/java/com/jpmc/cib/bps/entity/TradeEventId.java
@@ -3,8 +3,10 @@ package com.jpmc.cib.bps.entity;
 import com.jpmc.cib.bps.dto.EventType;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class TradeEventId {
+public class TradeEventId implements Serializable {
 
     private Long eventId;
     private EventType eventType;
